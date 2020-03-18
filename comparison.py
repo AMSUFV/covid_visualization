76ce03dc5042f20bc272f94f4ebde63a8cadfb8b
Angel Melchor <angelsk8mw@gmail.com>
2020-03-18 17:28:32 +0100

    implemented lineal, clipped movement

--- comparison.py
+++ comparison.py
@@ -3,8 +3,6 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 plt.style.use('seaborn')
 
-displacement = 0.005
-
 population_size = 100
 # patient zero
 patient_zero = np.random.randint(0, population_size)
@@ -15,6 +13,9 @@ population = np.zeros(population_size)
 population[patient_zero] = 1
 
 locations = np.random.rand(population_size, 2)
+
+displacement = 0.005
+displacement_matrix = (2 * displacement) * np.random.rand(population_size, 2) - displacement
 
 # four states: healthy (0), infected (1), cured (2), deceased (3)
 states = [x for x in range(4)]
@@ -39,11 +40,19 @@ def animate(*args):
             axs.set_xlim(0, 1)
             axs.set_ylim(0, 1)
 
-    # movement
-    movement = (2 * displacement) * np.random.rand(population_size, 2) - displacement
-    locations = locations + movement
-    locations = np.where(locations > 1, -(locations - 1) + 1, locations)
-    locations = abs(locations)
+    # random movement
+    # movement = (2 * displacement) * np.random.rand(population_size, 2) - displacement
+    # locations = locations + movement
+    # locations = np.where(locations > 1, -(locations - 1) + 1, locations)
+    # locations = abs(locations)
+
+    locations += displacement_matrix
+    # out of bounds check
+    x_coords = np.where(locations[:, 0] > 1, 0, locations[:, 0])
+    x_coords = np.where(x_coords < 0, 1, x_coords)
+    y_coords = np.where(locations[:, 1] > 1, 0, locations[:, 1])
+    y_coords = np.where(y_coords < 0, 1, y_coords)
+    locations = np.array(list(zip(x_coords, y_coords)))
 
 
 ani = animation.FuncAnimation(fig, animate, interval=25)
